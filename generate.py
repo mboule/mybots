a86b2c9c6ce6d02280545d3618b21212f9b888d9
mboule <matthew.boule@uvm.edu>
2021-02-18 22:38:26 -0500

    finished tower of links

--- generate.py
+++ generate.py
@@ -1,8 +1,21 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = 0.5
+
+
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z+i] , size=[length,width,height])
+    length *= .9
+    width *= .9
+    height *= .9
 
 pyrosim.End()
 
--- simulation.py
+++ simulation.py
@@ -1,9 +1,19 @@
 import pybullet as p
 import time
+import pybullet_data
+
 
 physicsClient = p.connect(p.GUI)
 
-p.loadSDF("box.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+
+planeId = p.loadURDF("plane.urdf")
+
+p.setGravity(0,0,-9.8)
+
+
+p.loadSDF("boxes.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
